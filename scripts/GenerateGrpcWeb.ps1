8c7fa0aac3fe02bf1af2e51f2d76970c533b860c
Steffen70 <steffen@seventy.mx>
2024-03-04 15:51:19 +0100

    Added generated gRPC-Web client files and the script for their generation.

---
+++ scripts/GenerateGrpcWeb.ps1
@@ -0,0 +1,30 @@
+param (
+    [string]$ProtoFilesPath,
+    [string]$OutputPath
+)
+
+$directoryPath = (Resolve-Path -Relative $PSScriptRoot)
+
+if(-not $ProtoFilesPath) {
+    $ProtoFilesPath = Resolve-Path (Join-Path $directoryPath "../protos")
+}
+
+if(-not $OutputPath) {
+    $OutputPath = Join-Path $directoryPath "../client_side/src/generated"
+}
+
+if (-not (Test-Path -Path $OutputPath -PathType Container)) {
+    New-Item -ItemType Directory -Path $OutputPath
+}
+
+$OutputPath = Resolve-Path $OutputPath
+
+$protoFiles = Get-ChildItem -Path $ProtoFilesPath -Filter *.proto
+
+foreach ($protoFile in $protoFiles) {
+    $protocCommand = "protoc --proto_path=$ProtoFilesPath --js_out=import_style=commonjs:$OutputPath --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OutputPath $($protoFile.FullName)"
+
+    Invoke-Expression $protocCommand
+}
+
+Write-Host "gRPC-Web client code generation completed."
